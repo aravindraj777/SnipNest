2a689dd70c171672688192e1ec2ac3616e030bbd
Aravind <aravindmunna240@gmail.com>
2024-09-22 19:12:04 +0530

    initial-commit

---
+++ src/main/java/com/buildlive/snipNest/SnipNestApplication.java
@@ -0,0 +1,13 @@
+package com.buildlive.snipNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildlive/snipNest/SnipNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildlive.snipNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
